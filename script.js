6739139db0357d119b8a64e5efdda37ffb02c34f
joannamhead <joannamhead@hotmail.co.uk>
2024-01-12 18:43:34 +0000

    added api key

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const apiKey = '4321925998421f25dc4380154a8ce64c';
-const queryURL = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4321925998421f25dc4380154a8ce64c';
 
